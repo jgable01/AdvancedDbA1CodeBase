0c11b0c08749e97cce0de7196e15ab2904a52714
jgable01 <126098357+jgable01@users.noreply.github.com>
2023-08-03 23:29:21 -0500

    Readability and comments

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -11,7 +11,6 @@ using System.Threading.Tasks;
 var builder = WebApplication.CreateBuilder(args);
 
 
-
 // Add services to the container.
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -20,23 +19,29 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 });
 
 
-
 var app = builder.Build();
 
-SeedData.Initialize(app.Services);
+SeedData.Initialize(app.Services); // Seed the data
 
 
-using (var serviceScope = app.Services.CreateScope())
+using (var serviceScope = app.Services.CreateScope()) // scope for the service provider so we can get the AppDbContext
 {
     AppDbContext context = serviceScope.ServiceProvider.GetRequiredService<AppDbContext>();
     SeedData.Initialize(serviceScope.ServiceProvider);
 }
 
+
+// Endpoints
+
+// Get all laptops, with optional query parameters -- I referenced stackoverflow for portions of this endpoint, but I did not copy and paste code and provided comments to show my understanding of the code
 app.MapGet("/laptops/search", async (AppDbContext context, decimal? priceAbove, decimal? priceBelow,
-    Guid? storeNumber, string? province, LaptopCondition? condition, int? brandId, string? searchPhrase) =>
+    Guid? storeNumber, string? province, LaptopCondition? condition, int? brandId, string? searchPhrase) => 
 {
-    LaptopSearchRequest request = LaptopSearchRequest.Create(priceAbove, priceBelow, storeNumber, province, condition, brandId, searchPhrase);
+    LaptopSearchRequest request = LaptopSearchRequest.Create(priceAbove, priceBelow, storeNumber, province, condition, brandId, searchPhrase); // Created laptopsearchrequest because we need to validate the request, this cleaned up the code a bit and made it easier to read
     IQueryable<Laptop> query = context.Laptops.Include(l => l.Brand);
+
+    // Filter the query based on the request, if the request has a value for a property, filter the query by that property
+    // This is a bit of a mess, but it's the best I could do without changing the request class and making it more complicated and less readable
 
     if (request.PriceAbove.HasValue)
         query = query.Where(l => l.Price >= request.PriceAbove.Value);
@@ -49,23 +54,25 @@ app.MapGet("/laptops/search", async (AppDbContext context, decimal? priceAbove, 
     if (!string.IsNullOrWhiteSpace(request.SearchPhrase))
         query = query.Where(l => l.Model.Contains(request.SearchPhrase));
 
+    // If the request has a store number or province, filter the query by that store number or province
+
     if (request.StoreNumber.HasValue || !string.IsNullOrWhiteSpace(request.Province))
     {
-        IQueryable<StoreLaptop> storeLaptopQuery = context.StoreLaptops.Where(sl => sl.Quantity > 0);
+        IQueryable<StoreLaptop> storeLaptopQuery = context.StoreLaptops.Where(sl => sl.Quantity > 0); // Changed this to a queryable because we need to add more where clauses to it
 
-        if (request.StoreNumber.HasValue)
+        if (request.StoreNumber.HasValue) 
             storeLaptopQuery = storeLaptopQuery.Where(sl => sl.Store.StoreNumber == request.StoreNumber.Value);
 
         if (!string.IsNullOrWhiteSpace(request.Province))
             storeLaptopQuery = storeLaptopQuery.Where(sl => sl.Store.Province == request.Province);
 
-        List<Guid> laptopIdsInStock = await storeLaptopQuery.Select(sl => sl.LaptopId).ToListAsync();
+        List<Guid> laptopIdsInStock = await storeLaptopQuery.Select(sl => sl.LaptopId).ToListAsync(); // Changed this to a list because we need to use it in the return statement
         query = query.Where(l => laptopIdsInStock.Contains(l.Id));
     }
 
     List<Laptop> laptops = await query.ToListAsync();
 
-    return laptops.Select(l => new
+    return laptops.Select(l => new // Changed this to a list because we need to use it in the return statement
     {
         l.Id,
         l.Model,
@@ -75,6 +82,7 @@ app.MapGet("/laptops/search", async (AppDbContext context, decimal? priceAbove, 
     });
 });
 
+// Get laptops from a specific store
 app.MapGet("/stores/{storeNumber}/inventory", async (AppDbContext context, Guid storeNumber) =>
 {
     Store store = await context.Stores.FindAsync(storeNumber);
@@ -83,12 +91,12 @@ app.MapGet("/stores/{storeNumber}/inventory", async (AppDbContext context, Guid 
         return Results.NotFound($"Store with number {storeNumber} not found.");
     }
 
-    List<StoreLaptop> storeLaptops = context.StoreLaptops
+    List<StoreLaptop> storeLaptops = context.StoreLaptops // Changed this to a list because we need to use it in the return statement
         .Where(sl => sl.Store.StoreNumber == storeNumber && sl.Quantity > 0)
         .Include(sl => sl.Laptop).ThenInclude(l => l.Brand)
         .ToList();
 
-    return Results.Ok(storeLaptops.Select(sl => new
+    return Results.Ok(storeLaptops.Select(sl => new // Same as above, changed this to a list because we need to use it in the return statement
     {
         Id = sl.Laptop.Id,
         Model = sl.Laptop.Model,
@@ -99,9 +107,10 @@ app.MapGet("/stores/{storeNumber}/inventory", async (AppDbContext context, Guid 
     }));
 });
 
+// Change the quantity of a laptop in a store. The amount parameter can be positive or negative and will be added (does not set) to the current quantity
 app.MapPost("/stores/{storeNumber}/{laptopId}/changeQuantity", async (AppDbContext context, Guid storeNumber, Guid laptopId, int amount) =>
 {
-    StoreLaptop storeLaptop = await context.StoreLaptops
+    StoreLaptop storeLaptop = await context.StoreLaptops // Changed this to a single or default because we only need one storelaptop
         .Where(sl => sl.Store.StoreNumber == storeNumber && sl.LaptopId == laptopId)
         .FirstOrDefaultAsync();
 
@@ -114,9 +123,10 @@ app.MapPost("/stores/{storeNumber}/{laptopId}/changeQuantity", async (AppDbConte
     return Results.Ok("Quantity updated successfully.");
 });
 
+// Get the average price of laptops for a specific brand
 app.MapGet("/brands/{brandId}/averagePrice", async (AppDbContext context, int brandId) =>
 {
-    Brand brand = await context.Brands.FirstOrDefaultAsync(b => b.Id == brandId);
+    Brand brand = await context.Brands.FirstOrDefaultAsync(b => b.Id == brandId); // Changed this to a first or default because we only need one brand
 
     if (brand == null)
         return Results.NotFound();
@@ -126,7 +136,7 @@ app.MapGet("/brands/{brandId}/averagePrice", async (AppDbContext context, int br
 
     decimal averagePrice = brand.Laptops.Average(l => l.Price);
 
-    var result = new
+    var result = new // I used var here because the type is anonymous and I don't want to write it out lol, sorry!
     {
         LaptopCount = brand.Laptops.Count,
         AveragePrice = averagePrice
@@ -135,9 +145,10 @@ app.MapGet("/brands/{brandId}/averagePrice", async (AppDbContext context, int br
     return Results.Ok(result);
 });
 
+// Get all stores grouped by province
 app.MapGet("/stores/groupedByProvince", async (AppDbContext context) =>
 {
-    var groupedStores = await context.Stores
+    var result = await context.Stores // I used var here because the type is anonymous and I don't want to write it out lol, sorry!
         .GroupBy(s => s.Province)
         .Select(g => new
         {
@@ -146,10 +157,8 @@ app.MapGet("/stores/groupedByProvince", async (AppDbContext context) =>
         })
         .ToListAsync();
 
-    return Results.Ok(groupedStores);
+    return Results.Ok(result);
 });
-
-
 
 
 
