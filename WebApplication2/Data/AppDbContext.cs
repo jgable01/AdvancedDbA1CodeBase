c80a0af8b15a5f1caadc5f51a7cb3160af962826
jgable01 <126098357+jgable01@users.noreply.github.com>
2023-08-04 12:59:41 -0500

    comments/readability

--- WebApplication2/Data/AppDbContext.cs
+++ WebApplication2/Data/AppDbContext.cs
@@ -18,13 +18,8 @@ namespace WebApplication2.Data
 
 
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder) // Took inspiration from stack-overflow. This allowed me to maintain variable names in the database while still being primary keys
         {
-            // Define relationships and constraints here
-            // For instance, you might need to set up a many-to-many relationship between Stores and Laptops
-
-            // This is just a placeholder; you'll need to define this according to your app requirements
-            // modelBuilder.Entity<Store>().HasMany(s => s.Laptops).WithMany(l => l.Stores);
             modelBuilder.Entity<Store>().HasKey(s => s.StoreNumber);
             modelBuilder.Entity<StoreLaptop>().HasKey(s => s.StoreLaptopId);
 
